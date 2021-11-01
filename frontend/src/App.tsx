c7fb588873e9553c7559d4aa1748905846115ce0
GabrielGui13 <gabrielguilherme13@hotmail.com>
2021-11-01 18:56:45 -0300

    Basic static components to DataTable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import DataTable from "./components/DataTable";
 
-
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1 className="text-primary">Hello World</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Hello World</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
